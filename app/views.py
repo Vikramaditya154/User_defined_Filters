4888be9d95875373bc58cd1a3cb545be292b2de5
Vikramaditya154 <arjunroyal6385@gmail.com>
2023-07-25 10:18:48 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def User_Defined_Filters(request):
+    d={'data':'Today we are learning User defined Filters'}
+    return render(request, 'User_Define_Filters.html',d)
